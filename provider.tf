230d0ec0a63ea61011d6cf793c2f40f57344f74b
Vanshika2004-yadav <vanshikayadav1042@gmail.com>
2026-05-29 11:22:43 +0530

    Enable Terraform apply with remote backend

--- provider.tf
+++ provider.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "s3" {
+    bucket = "vanshika-terraform-state-bucket-2026"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
